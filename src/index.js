91f9928059a1d67876435138d29cd9e534c728fc
Paola Miramontes <pmiramonteso@gmail.com>
2025-02-07 17:53:10 +0100

    redirigo hacia el frontend

--- src/index.js
+++ src/index.js
@@ -1,26 +1,29 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const authRutas = require('./routes/authRutas');
 const usuarioRutas = require('./routes/usuarioRutas');
 const testRoutes = require('./routes/testRutas');
 const memeRutas = require('./routes/memeRutas');
-const categoriaRutas = require('./routes/categoriaRutas')
+const categoriaRutas = require('./routes/categoriaRutas');
 const votosRutas = require('./routes/votosRutas');
 const apiKeyRutas = require('./routes/apiKeyRutas');
+
 const { verificarApiKey } = require('./middlewares/verificarApiKey');
 const { testConnection } = require('./db');
 const { authenticateToken } = require('./middlewares/authenticateToken');
+
 const { insertInitialUserData } = require('./start_data');
 const dotenv = require('dotenv');
-const path = require('path');
+dotenv.config();
 
 const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:4200',
+  origin: 'http://lasociedadelmeme.com',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
@@ -37,6 +40,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/assets/img', express.static(path.join(__dirname, '/uploads')));
 
+// Servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, '/lasociedadelmeme')));
 
 // Rutas administrativas
 app.use('/api/admin/memes', authenticateToken(['admin']), memeRutas);
@@ -44,17 +49,22 @@ app.use('/api/admin/categorias', authenticateToken(['admin']), categoriaRutas);
 
 // Rutas públicas
 app.use('/auth', authRutas);
-//app.use('/api/docs');
 app.use('/api', categoriaRutas);
-app.use('/api', memeRutas); // Rutas de demostración pública
+app.use('/api', memeRutas);
 
 // Rutas protegidas por usuario
 app.use('/usuario', usuarioRutas);
-app.use('/api/keys', apiKeyRutas); // Gestión de API Keys
+app.use('/api/keys', apiKeyRutas);
 
 // Rutas protegidas por API Key
-app.use('/api/v1', verificarApiKey, memeRutas); // API para desarrolladores
+app.use('/api/v1', verificarApiKey, memeRutas);
 
-app.listen(3000, () => {
-  console.log(`Servidor de Express escuchando en el puerto 3000`);
+// Redirigir todas las rutas no específicas de la API a index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/lasociedadelmeme/index.html'));
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Servidor de Express escuchando en el puerto ` + PORT);
 });
