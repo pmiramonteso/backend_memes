5640b0b535a3a64675c8d96a0d932786a71d0f4d
Paola Miramontes <pmiramonteso@gmail.com>
2025-02-10 14:52:05 +0100

    actualizo index.js por esmodule

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
-var http = require('http');
-var server = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var message = 'It works!\n',
-        version = 'NodeJS ' + process.versions.node + '\n',
-        response = [message, version].join('\n');
+import http from 'http';
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    const message = 'It works!\n';
+    const version = 'NodeJS ' + process.versions.node + '\n';
+    const response = [message, version].join('\n');
     res.end(response);
 });
-server.listen();
+
+server.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
+
