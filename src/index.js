e277c576a1b29ccb0d5ec4bce556ceed0ab288fc
Paola Miramontes <pmiramonteso@gmail.com>
2025-02-12 19:44:00 +0100

    Actualizacion dirname

--- src/index.js
+++ src/index.js
@@ -1,29 +1,21 @@
-// Reemplaza `import` por `require`
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
-const { fileURLToPath } = require('url');
 const dotenv = require('dotenv');
 
 const authRutas = require('./routes/authRutas.js');
 const usuarioRutas = require('./routes/usuarioRutas.js');
-const testRoutes = require('./routes/testRutas.js');
 const memeRutas = require('./routes/memeRutas.js');
 const categoriaRutas = require('./routes/categoriaRutas.js');
-const votosRutas = require('./routes/votosRutas.js');
 const apiKeyRutas = require('./routes/apiKeyRutas.js');
 
 const { verificarApiKey } = require('./middlewares/verificarApiKey.js');
 const { testConnection, sequelize } = require('./db.js');
 const { authenticateToken } = require('./middlewares/authenticateToken.js');
-const { insertInitialUserData } = require('./start_data.js');
 
 // Configuración de dotenv
 dotenv.config();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -37,13 +29,15 @@ sequelize.sync({ alter: true }).then(() => {
 // Middlewares
 app.use(cors({
   credentials: true,
-  origin: ['https://lasociedadelmeme.com',
-  'http://lasociedadelmeme.com',
-  'http://localhost:4200',
-  'https://localhost:4200'],
+  origin: [
+    'https://lasociedadelmeme.com',
+    'http://lasociedadelmeme.com',
+    'http://localhost:4200',
+    'https://localhost:4200'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
-  exposedHeaders: ['Set-Cookie'],
+  exposedHeaders: ['Set-Cookie']
 }));
 
 app.use(cookieParser());
@@ -53,7 +47,6 @@ app.use(express.urlencoded({ extended: true }));
 const initializeApp = async () => {
   try {
     await testConnection();
-    //await insertInitialUserData();
     console.log('Conexión a base de datos establecida');
   } catch (error) {
     console.error('Error al inicializar la aplicación:', error);
@@ -70,15 +63,19 @@ app.use(express.static(path.resolve(__dirname, 'frontend')));
 // Rutas administrativas
 app.use('/api/admin/memes', authenticateToken(['admin']), memeRutas);
 app.use('/api/admin/categorias', authenticateToken(['admin']), categoriaRutas);
+
 // Rutas públicas
 app.use('/auth', authRutas);
 app.use('/api', categoriaRutas);
 app.use('/api', memeRutas);
+
 // Rutas protegidas por usuario
 app.use('/usuario', usuarioRutas);
 app.use('/api/keys', apiKeyRutas);
+
 // Rutas protegidas por API Key
 app.use('/api/v1', verificarApiKey, memeRutas);
+
 // Redirigir todas las rutas no específicas de la API a index.html
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'frontend', 'index.html'));
